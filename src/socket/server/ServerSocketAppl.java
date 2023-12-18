781bf347a1bb2d7920340a7a2acf4c95ac985586
VK <vit_ko@ukr.net>
2023-12-18 22:55:02 +0100

    delete comments, can't get user directly

--- src/socket/server/ServerSocketAppl.java
+++ src/socket/server/ServerSocketAppl.java
@@ -24,31 +24,13 @@ public class ServerSocketAppl {
                     System.out.println("Client host: " + socket.getInetAddress() + ":" + socket.getPort());
 
                     ClientHandler clientHandler = new ClientHandler(socket);
-                    executorService.execute(clientHandler);
-
-                    // this code is inside 'ClientHandler'
-    /*                InputStream inputStream = socket.getInputStream();
-                    OutputStream outputStream = socket.getOutputStream();
-
-                    PrintWriter socketWriter = new PrintWriter(outputStream);
-                    BufferedReader socketReader = new BufferedReader(new InputStreamReader(inputStream));
-
-                    while (true) {
-                        String message = socketReader.readLine();
-                        if (message == null){
-                            break;              // breaks only one while() cycle,
-                        }
-                        System.out.println("Server received: " + message);
-
-                        socketWriter.println(message + " " + LocalTime.now());
-                        socketWriter.flush();
-                    }*/
+                    executorService.execute(clientHandler); // some code is inside 'ClientHandler'
                 }
             } finally {
                 executorService.shutdown();
                 executorService.awaitTermination(1, TimeUnit.MINUTES);
             }
-        } catch (Exception e) { //  IOException e -> Exception e
+        } catch (Exception e) {     //  IOException e -> Exception e
             e.printStackTrace();
         }
     }
--- src/socket/server/task/ClientHandler.java
+++ src/socket/server/task/ClientHandler.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 
 public class ClientHandler implements Runnable {
     private Socket socket;
-    private String userName = "you";
+//    private String userName = "you";
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
@@ -22,26 +22,22 @@ public class ClientHandler implements Runnable {
             PrintWriter socketWriter = new PrintWriter(outputStream);
             BufferedReader socketReader = new BufferedReader(new InputStreamReader(inputStream));
 
-//            BufferedReader socketReader2 = new BufferedReader(new InputStreamReader(inputStream));
-//            userName = socketReader2.readLine();
-            userName = socketReader.readLine();
-                if (userName == null) {
-//                    break;              // breaks only one while() cycle,
-                    userName = "you";
-                }
-            System.out.println("userName received: " + userName);
+//            userName = socketReader.readLine();
+//                if (userName == null) {
+//                    userName = "you";
+//                }
+//            System.out.println("userName received: " + userName);
 
             while (true) {
                 String message = socketReader.readLine();
                 if (message == null) {
                     break;              // breaks only one while() cycle,
                 }
-
                 System.out.println("Server received: " + message);
 
-                // Форматировать время
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm:ss");
 
+                //! can't get user directly
 //                socketWriter.println("At " + LocalTime.now().format(formatter) + " " + userName + " wrote: " + message +
 //                        " " + LocalTime.now());
                 socketWriter.println("At " + LocalTime.now().format(formatter) + " " + message + " " + LocalTime.now());
